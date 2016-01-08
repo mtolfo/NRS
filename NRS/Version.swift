18ae7f11bc6e6826570fd4a77b92515556cae0ae
Michael Tolfo <mtolfo@mac.com>
2016-01-08 08:54:05 -0500

    Add Version class

---
+++ NRS/Version.swift
@@ -0,0 +1,23 @@
+//
+//  Version.swift
+//  NRS
+//
+//  Created by Michael Tolfo on 1/8/16.
+//  Copyright © 2016 tolfosoftware. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Version
+{
+    var versionId = ""
+    var version = ""
+    
+    
+    init (version: String)
+    {
+        self.version = version
+    }
+}
+
