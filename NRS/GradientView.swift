e086593342f2598202e1e7a67bb73c235be166f9
Michael Tolfo <mtolfo@mac.com>
2016-04-06 20:54:57 -0400

    Gradient can rotate now. Problem is gradient will not extend to orange at the bottom. Does not work with iPad most likely because the device scale is incorrect.

---
+++ NRS/GradientView.swift
@@ -0,0 +1,77 @@
+//
+//  GradientView.swift
+//  NRS
+//
+//  Created by Michael Tolfo on 4/6/16.
+//  Copyright © 2016 tolfosoftware. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override class func layerClass() -> AnyClass {
+        return CAGradientLayer.self
+    }
+    
+    func gradientWithColors(firstColor : UIColor, _ secondColor : UIColor) {
+        
+        let deviceScale = UIScreen.mainScreen().scale
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = CGRectMake(0.0, 0.0, self.frame.size.width * deviceScale, self.frame.size.height * deviceScale)
+        gradientLayer.colors = [ firstColor.CGColor, secondColor.CGColor ]
+        
+        self.layer.insertSublayer(gradientLayer, atIndex: 0)
+    }
+    
+    func gradientWithHexColor()
+    {
+        let deviceScale = UIScreen.mainScreen().scale
+        
+        let gradientLayer = CAGradientLayer().orangeRedGradient()
+        gradientLayer.frame = CGRectMake(0.0, 0.0, self.frame.size.width * deviceScale, self.frame.size.height * deviceScale)
+
+        self.layer.insertSublayer(gradientLayer, atIndex: 0)
+        print (deviceScale)
+        print(self.frame.size.width * deviceScale)
+
+    }
+    
+//    func orangeRedGradient() -> CAGradientLayer
+//    {
+//        let gradientLayer:CAGradientLayer = CAGradientLayer()
+//        
+//        // SETS THE COLORS YOU WANT TO USE IN THE GRADIENT
+//        let color1 = UIColorFromRGB(0x9F041B).CGColor as CGColorRef
+//        let color2 = UIColorFromRGB(0xF09937).CGColor as CGColorRef
+//        
+//        // ADDS THE COLORS TO THE ARRAY
+//        gradientLayer.colors = [color1, color2]
+//        
+//        // SETS THE LOCATION OF THE RESPECTIVE COLORS
+//        gradientLayer.locations = [0.0,1]
+//        
+//        return gradientLayer
+//        
+//    }
+    
+    // RGB COLOR FUNCTION
+//    func UIColorFromRGB(rgbValue: UInt) -> UIColor
+//    {
+//        return UIColor(
+//            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+//            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+//            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+//            alpha: CGFloat(1.0)
+//        )
+//    }
+
+
+}
--- NRS/NewEvalSelectSessionViewController.swift
+++ NRS/NewEvalSelectSessionViewController.swift
@@ -14,62 +14,43 @@ class NewEvalSelectSessionViewController: UIViewController
     var blurEffectView:UIVisualEffectView?
     let gradientLayer = CAGradientLayer()
     
+    @IBOutlet weak var gradientView: GradientView!
+    @IBOutlet weak var gradientLayerView: UIView!
         
     override func viewDidLoad()
        {
         super.viewDidLoad()
         
-        // TODO - Delete this blur effect stuff if we don't need it
-        // Do any additional setup after loading the view.
-//        backgroundImagView.image = UIImage(named: "cloudNRSred")
-//        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
-//        blurEffectView = UIVisualEffectView(effect: blurEffect)
-//        blurEffectView?.frame = view.bounds
-//        backgroundImagView.addSubview(blurEffectView!)
         
-//        // to handle iPhone 4s screen size
-//        if UIScreen.mainScreen().bounds.size.height == 480.0
-//        {
-//            let flowLayout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-//            flowLayout.itemSize = CGSizeMake(250.0, 300.0)
-//        }
-        
-        // we created a CAGradientExtension here
-        let background = CAGradientLayer().orangeRedGradient()
-        background.frame = self.view.bounds
-        
-        
-        // INSERT GRADIENT LAYER BEHIND OTHER ELEMENTS
-        self.view.layer.insertSublayer(background, atIndex: 0)
-        
-        //font attributes
-//        labelRecoveryScale.backgroundColor = UIColor.redColor()
-//        labelNeuroMuscular.font = UIFont(name: "AvenirNext-UltraLight", size: 32)
-//        labelRecoveryScale.font = UIFont(name: "AvenirNext-UltraLight", size: 32)
-        
+//         //we created a CAGradientExtension here
+//        let background = CAGradientLayer().orangeRedGradient()
+//        background.frame = self.view.bounds
 //        
-//        let myTitle = NSAttributedString(string: "NeuroRecovery", attributes: [NSFontAttributeName:UIFont(name: "AvenirNext-UltraLight", size: 32.0)!,NSForegroundColorAttributeName:UIColor.whiteColor()])
-//        labelNeuroMuscular.attributedText = myTitle
-//        pickerLabel!.attributedText = myTitle
-//        pickerLabel!.textAlignment = .Center
 //        
-
+//        // INSERT GRADIENT LAYER BEHIND OTHER ELEMENTS
+//        self.view.layer.insertSublayer(background, atIndex: 0)
         
-//        for family: String in UIFont.familyNames()
-//        {
-//            print("\(family)")
-//                for names: String in UIFont.fontNamesForFamilyName(family)
-//                {
-//                    print("==\(names)")
-//                }
-//        }
         
+        
+    }
+   
+    
+    override func viewDidLayoutSubviews()
+    {
+        
+//        self.gradientView.gradientWithColors(UIColor(red: 159/255, green: 4/255, blue: 27/255, alpha: 1.0), UIColor(red: 240/255, green: 152/255, blue: 55/255, alpha: 1.0))
+        self.gradientView.gradientWithHexColor()
+        
+        //red 9F041B rgb  159 4 27
+        //orange F09937 240 153 55
     }
     
-    override func traitCollectionDidChange(previousTraitCollection: UITraitCollection?)
-    {
-        blurEffectView?.frame = view.bounds
-    }
+    
+    
+//    override func traitCollectionDidChange(previousTraitCollection: UITraitCollection?)
+//    {
+//        blurEffectView?.frame = view.bounds
+//    }
 
     override func didReceiveMemoryWarning()
     {
